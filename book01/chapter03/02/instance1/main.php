7a1ad2a9a8dc5abe2f464016b5e67b9249ed54f1
Shin Endo <end3mame@gmail.com>
2024-01-21 11:23:50 +0900

    インスタンスの追加

--- book01/chapter03/02/instance1/main.php
+++ book01/chapter03/02/instance1/main.php
@@ -14,5 +14,15 @@
         echo $task1->name, 'は未完了です。', PHP_EOL;
     }
 
+    // TODOタスク「食材の買い出し」を作る
+    $task2 = new Task();
+    $task2->name = '食材の買い出し';
+    $task2->progress = 50;
+    if($task2->isCompleted() === true ) {
+        echo $task2->name, 'は完了しました。', PHP_EOL;
+    } else {
+        echo $task2->name, 'は未完了です。', PHP_EOL;
+    }
+
 ?>
 </body>
