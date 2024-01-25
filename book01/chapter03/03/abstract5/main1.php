3c44ebbfe8ad3896a517f9e2f5a422cf6088b9c8
ShinEndo <endo@elplanning.co.jp>
2024-01-25 09:56:09 +0900

    継承におけるコンストラクタの扱い

---
+++ book01/chapter03/03/abstract5/main1.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1); ?>
+<body>
+<?php
+    class SuperClass {
+        protected $data1;
+        public function __construct(string $data1)
+        {
+            $this->data1 = $data1;
+            echo $this->data1;
+        }
+    }
+
+    class SubClass extends SuperClass {
+
+    }
+    $subClass = new SubClass('This is data1.');
+?>
+</body>
