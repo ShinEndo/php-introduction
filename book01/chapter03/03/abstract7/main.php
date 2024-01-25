d7f23f5dd1aabf6f2429f9958ae7aeb46ce85274
ShinEndo <endo@elplanning.co.jp>
2024-01-25 10:07:11 +0900

    スーパークラスのメソッドを呼び出す - parentキーワード

---
+++ book01/chapter03/03/abstract7/main.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1); ?>
+<body>
+<?php
+    abstract class Clock
+    {
+        public function __construct(int $time)
+        {
+            echo 'Clockクラスのコンストラクタが呼ばれました。', PHP_EOL;
+            $this->time = $time;
+        }
+    }
+
+    class DigitalClock extends Clock
+    {
+        public function __construct(int $time, string $color)
+        {
+            echo 'DigitalClockクラスのコンストラクタが呼ばれました。', PHP_EOL;
+            $this->color = $color;
+            parent::__construct($time);
+        }
+    }
+
+    // メインルーチン
+    $currentTime = strtotime('2018-08-22 17:15');
+    $digitalClock = new DigitalClock($currentTime,'yellow');
+?>
+</body>
