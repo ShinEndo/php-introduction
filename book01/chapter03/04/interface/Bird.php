1f34b80a35550f575831b037b2dcaa532947fb9d
ShinEndo <endo@elplanning.co.jp>
2024-01-26 10:29:41 +0900

    インターフェース

---
+++ book01/chapter03/04/interface/Bird.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(__FILE__) . '/Flyable.php';
+require_once dirname(__FILE__) . '/Walable.php';
+
+class Bird implements Flyable,Walkable
+{
+    public function fly(): void
+    {
+        echo 'Bird is flying..';
+    }
+
+    public function walk(): void
+    {
+        echo 'Bird is wakling..';
+    }
+}
